b2c8e439fcf539a77e7a1332512984c281187d3a
Egor <128175234+EgorShab@users.noreply.github.com>
2024-01-18 20:28:19 +0000

    Update report.component.ts

--- HalfDozenSurvey/client/src/app/survey/report/report.component.ts
+++ HalfDozenSurvey/client/src/app/survey/report/report.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-//import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { SurveyResponse } from 'src/app/model/surveyResponse.model';
 import { SurveyResponseRepository } from 'src/app/model/surveyResponse.service';
