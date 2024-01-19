283dc00adb34b551b986cedb3b319b4d834a6e44
Egor <128175234+EgorShab@users.noreply.github.com>
2024-01-19 03:40:21 +0000

    Update FoodItems.cs

--- RestaurantBillCalculator/src/FoodItems.cs
+++ RestaurantBillCalculator/src/FoodItems.cs
@@ -1,10 +1,4 @@
-﻿//using Microsoft.VisualBasic;
-//using System;
 using System.Collections.Generic;
-/* using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Documents; */
 
 namespace Lab3
 {
